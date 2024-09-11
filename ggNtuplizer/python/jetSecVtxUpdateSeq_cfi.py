2b0a12be2392dc34ac39e3814e26b53720b9b398
ltsai323 <johnson20050@gmail.com>
2024-09-11 09:13:24 +0200

    use slimmedJet instead of other jet. The newest version of slimmedJet seems to have enough information

--- ggNtuplizer/python/jetSecVtxUpdateSeq_cfi.py
+++ ggNtuplizer/python/jetSecVtxUpdateSeq_cfi.py
@@ -1,25 +1,28 @@
 import FWCore.ParameterSet.Config as cms
 
-from  PhysicsTools.PatAlgos.recoLayer0.jetCorrFactors_cfi import patJetCorrFactors
-jetCorrFactorsNano = patJetCorrFactors.clone(src='slimmedJets',
-    levels = cms.vstring('L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual'),
-    primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
-)
-
-from  PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cfi import updatedPatJets
-updatedJets = updatedPatJets.clone(
-	addBTagInfo=False,
-	jetSource='slimmedJets',
-	jetCorrFactorsSource=cms.VInputTag(cms.InputTag("jetCorrFactors") ),
-)
+#from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJetCorrFactors
+#jetCorrFactors = updatedPatJetCorrFactors.clone(
+#    src = cms.InputTag("slimmedJets"),
+#    primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+#    levels = ['L1FastJet', 'L2Relative', 'L3Absolute'],
+#    payload = 'AK4PFchs') 
+#
+#from  PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cfi import updatedPatJets
+#updatedJets = updatedPatJets.clone(
+#	addBTagInfo=False,
+#	jetSource='slimmedJets',
+#	jetCorrFactorsSource=cms.VInputTag(cms.InputTag("jetCorrFactors") ),
+#)
 bJetVars = cms.EDProducer("JetRegressionVarProducer",
     pvsrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    src = cms.InputTag("updatedJets"),
+    #src = cms.InputTag("updatedJets"),
+    src = cms.InputTag("slimmedJets"),
     svsrc = cms.InputTag("slimmedSecondaryVertices"),
     gpsrc = cms.InputTag("prunedGenParticles"),
 )
 updatedJetsWithUserData = cms.EDProducer("PATJetUserDataEmbedder",
-        src = cms.InputTag("updatedJets"),
+        #src = cms.InputTag("updatedJets"),
+        src = cms.InputTag("slimmedJets"),
         userFloats = cms.PSet(
             vtxPt = cms.InputTag("bJetVars:vtxPt"),
             vtxMass = cms.InputTag("bJetVars:vtxMass"),
@@ -30,4 +33,5 @@ updatedJetsWithUserData = cms.EDProducer("PATJetUserDataEmbedder",
             vtxNtrk = cms.InputTag("bJetVars:vtxNtrk"),
             ),
         )
-jetSecInfoUpdateSequence = cms.Sequence(jetCorrFactorsNano+updatedJets+bJetVars+updatedJetsWithUserData)
+jetSecInfoUpdateSequence = cms.Sequence(bJetVars+updatedJetsWithUserData)
+#jetSecInfoUpdateSequence = cms.Sequence(jetCorrFactors+updatedJets+bJetVars+updatedJetsWithUserData)
